bbee71cb653eea7b944f8ab46d363d4f4f1c0bf0
Reganvega <Reganvega@users.noreply.github.com>
2019-12-06 01:11:52 +1300

    INFO 281 Data analytics project

---
+++ Server.R
@@ -0,0 +1,12 @@
+#Loading up libraries for R
+library(tidyverse)
+library(shiny)
+library(ggplot2)
+library(plotly)
+
+#Setting work area
+setwd("C:/Users/regan/OneDrive/University Materials/2019/Summer/INFO281/Final Project/281 Project/Data")
+
+#Reading data for analysis
+readxl::excel_sheets("C:/Users/regan/OneDrive/University Materials/2019/Summer/INFO281/Final Project/281 Project/Data/Ethnic Data.xlsx")
+
